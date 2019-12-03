cb9c697fac0831cfc63218239553c08cb6e58972
GabrielHoffman <gabrielhoffman@typhon.wireless.mountsinai.org>
2019-12-03 15:40:00 -0500

    add new simulations

--- simulations_full.R
+++ simulations_full.R
@@ -182,7 +182,7 @@ sim_params = expand.grid( 	useResid 		= c(TRUE, FALSE),
 							n_samples 		= c(100, 200),
 							rho 			= c(.9), 
 							beta_disease 	= c(0),
-							beta_confounding= c(0, .5, 1, 3, 5, 10), 
+							beta_confounding= c(0, 5, 10, 50, 100), 
 							diffCorrScale 	= seq(1, 1.06, length.out=5)
 							)
 
